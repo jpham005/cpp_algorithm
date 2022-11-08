4fe41b6347a9d4a040a03b3080a56a5bb339a365
jpham005 <jpham005@naver.com>
2022-11-08 09:27:27 +0900

    nojam

---
+++ 7287.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <unistd.h>
+#include <cstdlib>
+
+using namespace std;
+
+int n, m;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cout << "159\njpham005\n";
+}
