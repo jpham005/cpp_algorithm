1b94d841332a91cdb9b3c1a6ec6bf2f2f978571a
jpham005 <jpham005@naver.com>
2022-11-16 15:16:34 +0900

    11286

---
+++ 11286.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int n;
+
+int heap[100000];
+int size = 0;
+
+bool compare(int a, int b) {
+  return (abs(a) < abs(b)) || (abs(a) == abs(b) && a < b);
+}
+
+void insert_heap(int x) {
+  heap[size++] = x;
+  int here = size;
+
+  while (here > 1 && compare(heap[here - 1], heap[here / 2 - 1])) {
+    swap(heap[here / 2 - 1], heap[here - 1]);
+    here /= 2;
+  }
+}
+
+void delete_heap() {
+  swap(heap[0], heap[--size]);
+
+  for (int curr = 1; curr * 2 <= size; ) {
+    if (compare(heap[curr * 2 - 1], heap[curr - 1])) {
+      if (curr * 2 + 1 <= size && compare(heap[curr * 2], heap[curr * 2 - 1])) {
+        swap(heap[curr - 1], heap[curr * 2]);
+        curr = curr * 2 + 1;
+        continue;
+      }
+
+      swap(heap[curr - 1], heap[curr * 2 - 1]);
+      curr *= 2;
+      continue;
+    }
+
+    if (curr * 2 + 1 <= size && compare(heap[curr * 2], heap[curr - 1]) ) {
+      swap(heap[curr - 1], heap[curr * 2]);
+      curr = curr * 2 + 1;
+      continue;
+    }
+
+    break;
+  }
+}
+
+void printall() {
+  for (int i = 0; i < size; i++) {
+    cout << heap[i] << ' ';
+  }
+  cout << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    int curr;
+    cin >> curr;
+
+    if (curr == 0) {
+      if (size == 0) {
+        cout << "0\n";
+        continue;
+      }
+
+      cout << heap[0] << '\n';
+      delete_heap();
+//      printall();
+      continue;
+    }
+
+    insert_heap(curr);
+//    printall();
+  }
+}
